e5495f92a8d4f9b65cfa5cef97772b4be9f0bff7
YAN Samreach <samreachyan@gmail.com>
2021-11-27 11:34:02 +0700

    add cmt

--- threading.py
+++ threading.py
@@ -1,8 +1,7 @@
 #!/bin/env python3
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty, InputPeerUser
-from telethon.errors.rpcerrorlist import PeerFloodError
+from telethon.tl.types import InputPeerEmpty
 from threading import Thread
 import configparser
 import os
@@ -26,107 +25,90 @@ print(f"""
             """)
 
 
-def send_sms(client, target_group):
-    '''Sending sms to group or channel'''
-    # Read message from text file
-    fileAttach = sys.argv[1]
-    with open(fileAttach, 'r') as f:
-        message = f.read()
-
-    try:
-        client.send_message(
-            target_group, message)
-        print(gr+"[+] Waiting {} seconds".format(SLEEP_TIME))
-        time.sleep(SLEEP_TIME)
-    except PeerFloodError:
-        print(re+"[!] Getting Flood Error from telegram. \n[!] Script is stopping now. \n[!] Please try again after some time.")
-
-        sys.exit()
-    except Exception as e:
-        print(re+"[!] Error:", e)
-        print(re+"[!] Trying to continue...")
-
-    print("Message sent to: " + target_group.title)
-
-
 def tasking():
     print("Thread started running")
 
 
-# get target group
-target_group = list()
-
-
-try:
-    cpass = configparser.RawConfigParser()
-    cpass.read('config.data')
-    api_id = cpass['cred']['id']
-    api_hash = cpass['cred']['hash']
-    phone = cpass['cred']['phone']
-except KeyError:
-    os.system('clear')
-    print(re+"[!] run python3 setup.py first !!\n")
-    sys.exit(1)
-
-client = TelegramClient(phone, api_id, api_hash)
-client.start()
-
-# get id group or channel
-chats = []
-last_date = None
-chunk_size = 100
-groups = []
-
-result = client(GetDialogsRequest(
-    offset_date=last_date,
-    offset_id=0,
-    offset_peer=InputPeerEmpty(),
-    limit=chunk_size,
-    hash=0
-))
-chats.extend(result.chats)
-
-for chat in chats:
+if __name__ == "__main__":
     try:
-        if True:
-            groups.append(chat)
-    except:
-        continue
+        cpass = configparser.RawConfigParser()
+        cpass.read('config.data')
+        api_id = cpass['cred']['id']
+        api_hash = cpass['cred']['hash']
+        phone = cpass['cred']['phone']
+    except KeyError:
+        os.system('clear')
+        print(re+"[!] run python3 setup.py first !!\n")
+        sys.exit(1)
 
-# print group or channels
-i = 0
-for g in groups:
-    print(gr+'['+cy+str(i)+gr+']'+cy+' - ' + g.title)
-    i += 1
+    client = TelegramClient(phone, api_id, api_hash)
+    client.start()
 
-print('')
-g_index = input(gr+"[+] Enter a Number : "+re)
-target_group = groups[int(g_index)]
-# finishing getting id target_group
+    # get id group or channel
+    chats = []
+    last_date = None
+    chunk_size = 100
+    groups = []
 
-message = "text.txt"
-with open(message, 'r', encoding="utf8") as f:
-    msg = f.read()
+    result = client(GetDialogsRequest(
+        offset_date=last_date,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=chunk_size,
+        hash=0
+    ))
+    chats.extend(result.chats)
 
-fileImage = "mytable.png"
+    for chat in chats:
+        try:
+            if True:
+                groups.append(chat)
+        except:
+            continue
 
-# print(target_group)
+    # print group or channels
+    i = 0
+    for g in groups:
+        print(gr+'['+cy+str(i)+gr+']'+cy+' - ' + g.title)
+        i += 1
 
-t1 = Thread(target=tasking)
-t1.start()
+    print('')
+    g_index = input(gr+"[+] Enter a Number : "+re)
+    target_group = groups[int(g_index)]
+    # finishing getting id target_group
 
-while True:
-    try:
-        now = datetime.datetime.now()
-        print("Current date and time : ")
-        print(now.strftime("%Y-%m-%d %H:%M:%S"))
-        client.send_message(
-            target_group, msg, file=fileImage)
-        time.sleep(60 * 2)
-    except KeyboardInterrupt:
-        print("stopping")
-        break
+    message = "text.txt"
+    with open(message, 'r', encoding="utf8") as f:
+        msg = f.read()
 
-t1.join()
-client.disconnect()
-print("done")
+    # Read image file
+    fileImage = "mytable.png"
+
+    # print(target_group)
+
+    t1 = Thread(target=tasking)
+    t1.start()
+
+    while True:
+        try:
+            # Connect DB function
+
+            # Save data to file.csv
+
+            # Convert to PNG
+
+            # Send to target_group
+            now = datetime.datetime.now()
+            print("Current date and time : ")
+            print(now.strftime("%Y-%m-%d %H:%M:%S"))
+            client.send_message(
+                target_group, msg, file=fileImage)
+            time.sleep(60 * 2)
+        except KeyboardInterrupt:
+            print("Stopping Thread")
+            break
+
+    # finish and disconnect to Telegram
+    t1.join()
+    client.disconnect()
+    print("Stopped Thread python")
